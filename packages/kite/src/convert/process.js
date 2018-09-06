3eab5a88d839e54c78c4375554fcbfcf5f9feb5d
Jared Henderson <jared@netrivet.com>
2018-09-06 15:21:34 -0400

    kite: escape periods after paragraphs that start with year

--- packages/kite/src/convert/process.js
+++ packages/kite/src/convert/process.js
@@ -5,6 +5,7 @@ import type { Asciidoc } from '../type';
 export function processAsciidoc(adoc: Asciidoc): Asciidoc {
   return adoc
     .replace(/(?<!footnote:)\[/gm, '+++[+++')
+    .replace(/\n\n(1[678][0-9]{2})\. /gm, '\n\n$1+++.+++ ')
     .replace(/\n\n----*\n\n/gm, '\n\n[.asterism]\n\'\'\'\n\n')
     .replace(/(____*)/g, '+++$1+++')
     .replace(/\n\n([A-Z])\. ([A-Z])/gm, '\n\n$1+++.+++ $2')
