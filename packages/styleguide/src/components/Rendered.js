c7dd2d505a0bfef65d7117f576722dfe4eeef5d4
Jared Henderson <jared@netrivet.com>
2018-10-29 11:59:09 -0400

    wip more styleguide

--- packages/styleguide/src/components/Rendered.js
+++ packages/styleguide/src/components/Rendered.js
@@ -2,21 +2,22 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
+import frags from '../../dist/frags.json';
 
-let doctor;
-const convert = (adoc) => {
-  if (!doctor) {
-    doctor = new window.Asciidoctor();
-  }
-  return doctor.convert(adoc);
-}
+// let doctor;
+// const convert = (adoc) => {
+//   if (!doctor) {
+//     doctor = new window.Asciidoctor();
+//   }
+//   return doctor.convert(adoc);
+// }
 
 const StyledDiv = styled.div`
-  background: white;
+  background: green;
   font-family: Georgia;
   padding: 1em;
 `;
 
 export default ({ adoc }) => (
-  <StyledDiv className="rendered-adoc" dangerouslySetInnerHTML={{__html: convert(adoc)}}/>
+  <StyledDiv className="rendered-adoc" dangerouslySetInnerHTML={{__html: frags.foo }} />
 );
