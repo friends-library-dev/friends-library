600b3f615ad4f85e72b3f1d3e9299118dc45be80
Jared Henderson <jared@netrivet.com>
2018-11-30 15:47:21 -0500

    wip

--- packages/site/plugins/gatsby-source-friends-library/gatsby-node.js
+++ packages/site/plugins/gatsby-source-friends-library/gatsby-node.js
@@ -1,4 +1,4 @@
-const { getFriend } = require('./friends/es5');
+const { getAllFriends, getFriend } = require('./friends/es5');
 
 exports.sourceNodes = (
   { actions, createNodeId, createContentDigest },
@@ -9,6 +9,20 @@ exports.sourceNodes = (
   // Gatsby adds a configOption that's not needed for this plugin, delete it
   delete configOptions.plugins
 
-  // plugin code goes here...
-  console.log("Testing my plugin", getFriend('rebecca-jones'));
+  // const allFriends = getAllFriends();
+  const allFriends = [getFriend('rebecca-jones')];
+  allFriends.forEach(friend => {
+    console.log(createNodeId(friend.slug));
+    console.log(JSON.stringify(friend));
+    createNode({
+      id: createNodeId(friend.slug),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Friend',
+        content: JSON.stringify(friend),
+        contentDigest: createContentDigest(friend),
+      },
+    });
+  });
 }
