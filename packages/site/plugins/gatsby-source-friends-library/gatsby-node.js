b2bec15e10cb9203e704bcf2ca4b45d5bcecdef7
Jared Henderson <jared@netrivet.com>
2018-11-30 11:06:08 -0500

    WIP scaffold gatsby with lol source plugin

---
+++ packages/site/plugins/gatsby-source-friends-library/gatsby-node.js
@@ -0,0 +1,12 @@
+exports.sourceNodes = (
+  { actions, createNodeId, createContentDigest },
+  configOptions
+) => {
+  const { createNode } = actions
+
+  // Gatsby adds a configOption that's not needed for this plugin, delete it
+  delete configOptions.plugins
+
+  // plugin code goes here...
+  console.log("Testing my plugin", configOptions)
+}
