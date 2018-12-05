c02545acf29e7cefd965d674fe9bc4ae4bb7f2c3
Jared Henderson <jared@netrivet.com>
2018-12-05 11:33:12 -0500

    wip: a little bit of flow

--- packages/site/gatsby-config.js
+++ packages/site/gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: [
+    'gatsby-plugin-flow',
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
--- packages/site/src/components/layout.js
+++ packages/site/src/components/layout.js
@@ -1,47 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+// @flow
+import * as React from 'react'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
 import './layout.css'
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet>
-          <html lang="en" />
-          <title>Friends Library</title>
-          <meta name="robots" content="noindex, nofollow" />
-        </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
-      </>
-    )}
-  />
+type Props = {|
+  children: React.Node,
+|};
+
+export default ({ children }: Props) => (
+  <>
+    <Helmet>
+      <html lang="en" />
+      <title>Friends Library</title>
+      <meta name="robots" content="noindex, nofollow" />
+    </Helmet>
+    <Header siteTitle="Friends Library is the best! 👍" />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      {children}
+    </div>
+  </>
 )
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
