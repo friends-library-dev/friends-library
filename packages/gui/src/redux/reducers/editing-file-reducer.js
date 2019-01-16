70e0aabbea3ef837272ed7847dcecb898be9ed72
Jared Henderson <jared@netrivet.com>
2019-01-15 20:28:11 -0500

    gui: delete, long live jones

--- packages/gui/src/redux/reducers/editing-file-reducer.js
+++
@@ -1,7 +0,0 @@
-// @flow
-import { createReducer } from 'redux-starter-kit';
-
-export default createReducer(null, {
-  SET_EDITING_FILE: (state, { payload }) => payload,
-  WORK_ON_TASK: () => null,
-});
--- packages/gui/src/redux/screens.js
+++
@@ -1,4 +0,0 @@
-// @flow
-export const TASKS = 'TASKS';
-export const EDIT_TASK = 'EDIT_TASK';
-export const WORK = 'WORK';
