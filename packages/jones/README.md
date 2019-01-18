1551e2518224789ec3f54ce90ddc8fc3eca11f8c
Jared Henderson <jared@netrivet.com>
2019-01-18 17:14:46 -0500

    jones: gracefully handle github api errors

--- packages/jones/README.md
+++ packages/jones/README.md
@@ -7,7 +7,6 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 ### Todo:
 
 * [ ] undo/redo stuff (omnidan/redux-undo -- on just `tasks`?)
-* [ ] network error recovery
 * [ ] communication state for app throbbing network things
 * [ ] delete task when PR merged (and task has no unpushed work)
 * [ ] multi-column view editor
@@ -17,6 +16,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 * [ ] allow custom PR body composition at submit time
 * [ ] allow custom commit message for re-submit commits
 * [ ] delete branch from fork when deleting task
+* [x] network error recovery
 * [x] dudley bug: funky character/encoding
 * [x] order editions in sidebar (updated, modernized, orig)
 * [x] launch super-basic search
--- packages/jones/src/actions.js
+++ packages/jones/src/actions.js
@@ -1,6 +1,7 @@
 // @flow
 import { createAction } from 'redux-starter-kit';
 import { safeLoad as ymlToJs } from 'js-yaml';
+import smalltalk from 'smalltalk';
 import type { Slug, Url, Asciidoc } from '../../../type';
 import type { Dispatch, State, Task, ReduxThunk, SearchResult } from './type';
 import * as gh from './lib/github-api';
@@ -86,12 +87,16 @@ export function submitTask(task: Task): ReduxThunk {
   return async (dispatch: Dispatch, getState: () => State) => {
     const { github: { user } } = getState();
     dispatch({ type: 'SUBMITTING_TASK' });
-    const pr = await gh.createNewPullRequest(task, user);
-    dispatch({ type: 'TASK_SUBMITTED', payload: {
-      id: task.id,
-      prNumber: pr.number,
-      parentCommit: pr.commit,
-    }});
+    const pr = await tryGithub(async () => {
+      return await gh.createNewPullRequest(task, user)
+    }, 'SUBMIT_TASK', dispatch);
+    if (pr) {
+      dispatch({ type: 'TASK_SUBMITTED', payload: {
+        id: task.id,
+        prNumber: pr.number,
+        parentCommit: pr.commit,
+      }});
+    }
   };
 }
 
@@ -99,57 +104,74 @@ export function resubmitTask(task: Task): ReduxThunk {
   return async (dispatch: Dispatch, getState: () => State) => {
     const { github: { user } } = getState();
     dispatch({ type: 'RE_SUBMITTING_TASK' });
-    const sha = await gh.addCommit(task, user);
-    dispatch({
-      type: 'TASK_RE_SUBMITTED',
-      payload: {
-        id: task.id,
-        parentCommit: sha,
-      }
-    });
+    const sha = await tryGithub(async () => {
+      return await gh.addCommit(task, user);
+    }, 'RESUBMIT_TASK', dispatch);
+    if (sha) {
+      dispatch({
+        type: 'TASK_RE_SUBMITTED',
+        payload: {
+          id: task.id,
+          parentCommit: sha,
+        }
+      });
+    }
   };
 }
-
 
 export function checkout(task: Task): ReduxThunk {
   return async (dispatch: Dispatch, getState: () => State) => {
     dispatch({ type: 'START_CHECKOUT' });
-    const repoSlug = await gh.getRepoSlug(task.repoId);
-    const parentCommit = await gh.getHeadSha(repoSlug, 'master');
-    let files = await gh.getAdocFiles(repoSlug, parentCommit);
-    files = files.reduce((acc, file) => {
-      acc[file.path] = file;
-      return acc;
-    }, {});
-    const yml = await fetch(friendYmlUrl(repoSlug)).then(r => r.text());
-    const { documents } = ymlToJs(yml);
-    const documentTitles = documents.reduce((acc, doc) => {
-      acc[doc.slug] = doc.title;
-      return acc;
-    }, {});
 
-    dispatch({ type: 'END_CHECKOUT' });
-    dispatch({
-      type: 'UPDATE_TASK',
-      payload: {
-        id: task.id,
-        data: { documentTitles, files, parentCommit }
-      }
-    });
+    const data = await tryGithub(async () => {
+      const repoSlug = await gh.getRepoSlug(task.repoId);
+      const parentCommit = await gh.getHeadSha(repoSlug, 'master');
+      let files = await gh.getAdocFiles(repoSlug, parentCommit);
+      files = files.reduce((acc, file) => {
+        acc[file.path] = file;
+        return acc;
+      }, {});
+      const yml = await fetch(friendYmlUrl(repoSlug)).then(r => r.text());
+      const { documents } = ymlToJs(yml);
+      const documentTitles = documents.reduce((acc, doc) => {
+        acc[doc.slug] = doc.title;
+        return acc;
+      }, {});
+      return Promise.resolve({ documentTitles, files, parentCommit });
+    }, 'CHECKOUT', dispatch);
+
+    if (data) {
+      dispatch({ type: 'END_CHECKOUT' });
+      dispatch({
+        type: 'UPDATE_TASK',
+        payload: {
+          id: task.id,
+          data,
+        }
+      });
+    } else {
+      dispatch({ type: 'CHANGE_SCREEN', payload: 'TASKS' });
+    }
   }
 }
 
 export function fetchFriendRepos() {
   return async (dispatch: Dispatch, getState: () => State) => {
     dispatch({ type: 'REQUEST_FRIEND_REPOS' });
-    const friendRepos = await gh.getFriendRepos();
-    // filter out any friend repos that don't have a yml file yet
-    const ymlsPath = '/repos/:owner/:repo/contents/packages/friends/yml/en';
-    const { data: ymls } = await gh.req(ymlsPath, { repo: 'friends-library' });
-    const filtered = friendRepos.filter(repo => {
-      return !!ymls.find(y => y.name === `${repo.name}.yml`);
-    });
-    dispatch({ type: 'RECEIVE_FRIEND_REPOS', payload: filtered });
+    let repos;
+    try {
+      const friendRepos = await gh.getFriendRepos();
+      // filter out any friend repos that don't have a yml file yet
+      const ymlsPath = '/repos/:owner/:repo/contents/packages/friends/yml/en';
+      const { data: ymls } = await gh.req(ymlsPath, { repo: 'friends-library' });
+      repos = friendRepos.filter(repo => {
+        return !!ymls.find(y => y.name === `${repo.name}.yml`);
+      });
+    } catch (e) {
+      dispatch({ type: 'NETWORK_ERROR' });
+      return;
+    }
+    dispatch({ type: 'RECEIVE_FRIEND_REPOS', payload: repos });
   }
 }
 
@@ -166,4 +188,30 @@ export function requestGitHubUser() {
       }
     });
   }
+}
+
+async function tryGithub(fn:(any) => *, errorType: string, dispatch): Promise<*> {
+  let result;
+  try {
+    // $FlowFixMe
+    result = await fn();
+  } catch (e) {
+    dispatch({ type: 'NETWORK_ERROR' });
+    alertGithubError(errorType);
+    return false;
+  }
+  return result;
+}
+
+function alertGithubError(type: string) {
+  smalltalk.alert(
+    '😬 <b style="color: red;">Network Error</b>',
+    ghErrorMsgs[type],
+  );
+}
+
+const ghErrorMsgs = {
+  SUBMIT_TASK: 'There was an error submitting your task to GitHub. Probably just a temporary glitch on their end. None of your work was lost, try submitting again in a few seconds. 🤞',
+  RESUBMIT_TASK: 'There was an error re-submitting your task to GitHub. Probably just a temporary glitch on their end. None of your work was lost, try submitting again in a few seconds. 🤞',
+  CHECKOUT: 'There was an error retrieving source files to edit. Probably just a temporary glitch with GitHub. Try again in a few seconds. 🤞',
 }
