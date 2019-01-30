d36158e002fbf6e8145170b9c5fb34a6dbe55c5d
Jared Henderson <jared@netrivet.com>
2019-01-30 16:19:53 -0500

    api: delete temp pdf preview on PR close

--- packages/api/src/pr-bot/__tests__/adoc-pr-tests.js
+++ packages/api/src/pr-bot/__tests__/adoc-pr-tests.js
@@ -1,6 +1,6 @@
 import * as friends from '@friends-library/friends';
 import stripIndent from 'strip-indent';
-import { adocPrCommitHandler } from '../adoc-pr';
+import { handleNewCommit } from '../adoc-pr';
 import * as gh from '../github';
 import * as pdf from '../pdf';
 import * as cloud from '../cloud';
@@ -10,7 +10,7 @@ jest.mock('../github');
 jest.mock('../pdf');
 jest.mock('../cloud');
 
-describe('adocPrCommitHandler()', () => {
+describe('handleNewCommit()', () => {
   let payload;
 
   beforeEach(() => {
@@ -31,13 +31,13 @@ describe('adocPrCommitHandler()', () => {
   });
 
   it('calls gh.getModifiedFiles with the right params', async () => {
-    await adocPrCommitHandler('pull_request', payload);
+    await handleNewCommit('pull_request', payload);
     expect(gh.getModifiedFiles).toBeCalledWith('jane-doe', 11);
   });
 
   it('calls gh.getPrFiles with right params', async () => {
     const { pull_request: { head: { sha } } } = payload;
-    await adocPrCommitHandler('pull_request', payload);
+    await handleNewCommit('pull_request', payload);
     expect(gh.getPrFiles).toBeCalledWith('jane-doe', sha);
   });
 
@@ -47,7 +47,7 @@ describe('adocPrCommitHandler()', () => {
     const prFiles = new Map([['file.adoc', '== Chapter 1']]);
     gh.getModifiedFiles.mockReturnValue(modifiedFiles);
     gh.getPrFiles.mockReturnValue(prFiles);
-    await adocPrCommitHandler('pull_request', payload);
+    await handleNewCommit('pull_request', payload);
     expect(friends.getFriend).toBeCalledWith('jane-doe');
     expect(pdf.createJobs).toBeCalledWith('FakeFriend', modifiedFiles, prFiles);
   });
@@ -55,13 +55,13 @@ describe('adocPrCommitHandler()', () => {
   it('calls pdf.createPdfs with created jobs', async () => {
     const jobs = ['my fake job'];
     pdf.createJobs.mockReturnValue(jobs);
-    await adocPrCommitHandler('pull_request', payload);
+    await handleNewCommit('pull_request', payload);
     expect(pdf.makePdfs).toBeCalledWith(jobs);
   });
 
   it('calls cloud.uploadFiles with the correct map of files', async () => {
     pdf.makePdfs.mockReturnValue(['/path/to/created.pdf']);
-    await adocPrCommitHandler('pull_request', payload);
+    await handleNewCommit('pull_request', payload);
     expect(cloud.uploadFiles).toBeCalledWith(new Map([[
       'adoc-pr/jane-doe/11/2d306bb70578e6c019e3579c02d4f78f17bf915e/created.pdf',
       '/path/to/created.pdf',
@@ -70,7 +70,7 @@ describe('adocPrCommitHandler()', () => {
 
   it('calls gh.updateableComment with correct params', async () => {
     cloud.uploadFiles.mockReturnValue(['http://foo.com/bar/some.pdf']);
-    await adocPrCommitHandler('pull_request', payload);
+    await handleNewCommit('pull_request', payload);
 
     const body = stripIndent(`
       PDF previews (commit 2d306bb70578e6c019e3579c02d4f78f17bf915e):
