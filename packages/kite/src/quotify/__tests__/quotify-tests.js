589af871602558c46c8ad42225c2fcc11fa69431
Jared Henderson <jared@netrivet.com>
2019-02-01 16:45:51 -0500

    kite: refine quotify to handle nested quoting

--- packages/kite/src/quotify/__tests__/quotify-tests.js
+++ packages/kite/src/quotify/__tests__/quotify-tests.js
@@ -1,115 +1,8 @@
+import fs from 'fs';
 import stripIndent from 'strip-indent';
 import quotify from '../quotify';
 
-const testCases = [
-  [
-    "got to Job Briggs',",
-    "got to Job Briggs`',",
-  ],
-  [
-    "lodged at Carlton Stokes'. That",
-    "lodged at Carlton Stokes`'. That",
-  ],
-  [
-    "Dined at Josiah Evans'. After",
-    "Dined at Josiah Evans`'. After",
-  ],
-  [
-    "when we got to David Binns', we",
-    "when we got to David Binns`', we",
-  ],
-  [
-    "Sadly, by the end of the 1800's, the Society",
-    "Sadly, by the end of the 1800`'s, the Society",
-  ],
-  [
-    "without understanding!' Fear ye not",
-    "without understanding!`' Fear ye not",
-  ],
-  [
-    "a quarter less three,'--and immediately",
-    "a quarter less three,`'--and immediately",
-  ],
-  [
-    '"`Blessed be the name of the Lord.`"\'',
-    '"`Blessed be the name of the Lord.`"`\'',
-  ],
-  [
-    'it should please the "faithful witness," in',
-    'it should please the "`faithful witness,`" in',
-  ],
-  [
-    'who cried--"Father, forgive',
-    'who cried--"`Father, forgive',
-  ],
-  [
-    'of the apostle,--"this is a faithful saying',
-    'of the apostle,--"`this is a faithful saying',
-  ],
-  [
-    'of agriculture?"--at which time a willingness',
-    'of agriculture?`"--at which time a willingness',
-  ],
-  [
-    '--"billy went to the farm',
-    '--"`billy went to the farm',
-  ],
-  [
-    "don't",
-    "don`'t",
-  ],
-  [
-    "Bob's leg hurts",
-    "Bob`'s leg hurts",
-  ],
-  [
-    '"`Foo bar.`"', // leaves already correct alone
-    '"`Foo bar.`"',
-  ],
-  [
-    'Foo bar,--"foo',
-    'Foo bar,--"`foo',
-  ],
-  [
-    '"Foo bar."',
-    '"`Foo bar.`"',
-  ],
-  [
-    'Foo "bar."',
-    'Foo "`bar.`"',
-  ],
-  [
-    'Foo "bar,"',
-    'Foo "`bar,`"',
-  ],
-  [
-    '"Foo bar."',
-    '"`Foo bar.`"',
-  ],
-  [
-    'Foo "bar."',
-    'Foo "`bar.`"',
-  ],
-  [
-    'Foo "bar,"',
-    'Foo "`bar,`"',
-  ],
-  [
-    '"Foo',
-    '"`Foo',
-  ],
-  [
-    "'Foo",
-    "'`Foo",
-  ],
-  [
-    '"Foo"',
-    '"`Foo`"',
-  ],
-  [
-    "'Foo'",
-    "'`Foo`'",
-  ],
+const multilineCases = [
   [
     strip(`
       This is a "multiline
@@ -130,20 +23,22 @@ const testCases = [
       inline quoation\`' foo bar.
     `),
   ],
-  [
-    'Foo bar "hash baz"',
-    'Foo bar "`hash baz`"',
-  ],
-  [
-    "Foo bar 'hash baz'",
-    "Foo bar '`hash baz`'",
-  ],
 ];
 
 
+const fixtureCases = fs.readFileSync(`${__dirname}/fixture.adoc`)
+  .toString()
+  .trim()
+  .split('\n\n')
+  .map(pair => pair.split('\n').map(line => line.trim()));
+
 describe('quotify()', () => {
-  test.each(testCases)('quotifies %s', (before, after) => {
+  test.each(fixtureCases)('quotifies %s', (before, after) => {
     expect(quotify(before)).toBe(after);
+  });
+
+  test.each(multilineCases)('quotifies %s', (before, after) => {
+    expect(quotify(before.replace(/\^/g, '`'))).toBe(after.replace(/\^/g, '`'));
   });
 
   test('asterism not quotified', () => {
