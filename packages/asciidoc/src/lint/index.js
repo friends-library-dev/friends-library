6c2ca75f367e6b091d55255ee0b1743be3063411
Jared Henderson <jared@netrivet.com>
2019-03-04 14:26:22 -0500

    asciidoc: linting comments

--- packages/asciidoc/src/lint/index.js
+++ packages/asciidoc/src/lint/index.js
@@ -8,6 +8,15 @@ export default function lint(adoc: Asciidoc): Array<LintResult> {
   const lineRules = values(lineLints);
   const lines = adoc.split('\n');
   const lineResults = lines.reduce((acc, line, index) => {
+    if (isLintComment(line)) {
+      return acc;
+    }
+
+    if (isComment(line)) {
+      acc.push(commentWarning(index + 1));
+      return acc;
+    }
+
     lineRules.forEach(rule => {
       const ruleResults = rule(line, lines, index + 1);
       ruleResults.forEach(result => acc.push(result));
@@ -21,4 +30,23 @@ export default function lint(adoc: Asciidoc): Array<LintResult> {
   }, []);
 
   return [...lineResults, ...blockResults];
+}
+
+function isLintComment(line: Asciidoc): boolean {
+  return isComment(line) && !!line.match(/lint-(disable|ignore)/);
+}
+
+
+function isComment(line: Asciidoc): boolean {
+  return line[0] === '/' && line[1] === '/';
+}
+
+function commentWarning(lineNumber: number): LintResult {
+  return {
+    line: lineNumber,
+    column: false,
+    type: 'warning',
+    rule: 'temporary-comments',
+    message: 'Comments should generally be removed, with the exceptions of: 1) comments to disable lint rules (e.g. `// lint-disable invalid-character`), and 2) special cases where there would be a long-term value to keeping the comment (these lines can be marked with `--lint-ignore` to disable this lint warning)',
+  };
 }
