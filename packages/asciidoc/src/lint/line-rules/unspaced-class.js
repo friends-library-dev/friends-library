3f117f97385c9892caff43eb58b1cec9a6d51f07
Jared Henderson <jared@netrivet.com>
2019-03-13 10:45:34 -0400

    asciidoc: add unspaced-class lint

---
+++ packages/asciidoc/src/lint/line-rules/unspaced-class.js
@@ -0,0 +1,28 @@
+// @flow
+import type { Asciidoc, LintResult } from '../../../../../type';
+
+export default function rule(
+  line: Asciidoc,
+  lines: Array<Asciidoc>,
+  lineNumber: number,
+): Array<LintResult> {
+  if (line === '' || line[0] !== '[' || line[line.length - 1] !== ']' || lineNumber === 1) {
+    return [];
+  }
+
+  if (lines[lineNumber - 2] === '') {
+    return [];
+  }
+
+  return [{
+    line: lineNumber,
+    column: false,
+    type: 'error',
+    rule: rule.slug,
+    message: 'Class/id designations (like `[.something]`) must be preceded by an empty line',
+    fixable: true,
+    recommendation: `--> add an empty line before line ${lineNumber}`,
+  }];
+}
+
+rule.slug = 'unspaced-class';
