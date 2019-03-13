da481bf30680f4435c62141316f757f1160882c6
Jared Henderson <jared@netrivet.com>
2019-03-12 21:28:44 -0400

    asciidoc: add capitalize lint rule

---
+++ packages/asciidoc/src/lint/line-rules/__tests__/capitalize-tests.js
@@ -0,0 +1,35 @@
+import capitalize from '../capitalize';
+
+describe('capitalize()', () => {
+  it('creates a lint for violation of `capitalize` rule', () => {
+    const results = capitalize('Wiles of satan', [], 1);
+    expect(results).toHaveLength(1);
+    expect(results[0]).toEqual({
+      line: 1,
+      column: 10,
+      type: 'error',
+      rule: 'capitalize',
+      message: '"Satan" should be capitalized everywhere in all editions',
+      recommendation: 'Wiles of Satan',
+      fixable: true,
+    });
+  });
+
+  const violations = [
+    ['Foo satan,', 'Foo Satan,'],
+  ];
+
+  test.each(violations)('`%s` should become "%s"', (line, reco) => {
+    const results = capitalize(line, [], 1);
+    expect(results).toHaveLength(1);
+    expect(results[0].recommendation).toBe(reco);
+  });
+
+  const allowed = [
+    ['Satan foo'],
+  ];
+
+  test.each(allowed)('%s is not a lint violation', line => {
+    expect(capitalize(line, [], 1)).toHaveLength(0);
+  });
+});
