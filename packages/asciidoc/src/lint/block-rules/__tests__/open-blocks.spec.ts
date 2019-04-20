a2e4b0788ca53b5202d6cf772ec4daecbb43a760
Jared Henderson <jared@netrivet.com>
2019-04-20 18:27:54 -0400

    asciidoc: lint open block delimiters missing surrounding newlines

--- packages/asciidoc/src/lint/block-rules/__tests__/open-blocks.spec.ts
+++ packages/asciidoc/src/lint/block-rules/__tests__/open-blocks.spec.ts
@@ -1,6 +1,89 @@
 import stripIndent from 'strip-indent';
 import openBlocks from '../open-blocks';
 
+describe('openBlocks() - missing-surrounding-space', () => {
+  it('lints closing terminator without proper spacing before', () => {
+    const adoc = stripIndent(`
+      Foo bar.
+
+      [.embedded-content-document.letter]
+      --
+
+      Foo bar
+      and baz.
+      --
+
+      Foo
+    `).trim();
+
+    const results = openBlocks(adoc);
+    expect(results).toHaveLength(1);
+    expect(results[0]).toEqual({
+      line: 8,
+      column: false,
+      type: 'error',
+      rule: 'open-block',
+      message: 'Open block delimiters should be surrounded by empty lines',
+      recommendation: '--> add an empty line before line 8',
+      fixable: true,
+    });
+  });
+
+  it('lints closing terminator without proper spacing after', () => {
+    const adoc = stripIndent(`
+      Foo bar.
+
+      [.embedded-content-document.letter]
+      --
+
+      Foo bar
+      and baz.
+
+      --
+      Foo
+    `).trim();
+
+    const results = openBlocks(adoc);
+    expect(results).toHaveLength(1);
+    expect(results[0]).toEqual({
+      line: 10,
+      column: false,
+      type: 'error',
+      rule: 'open-block',
+      message: 'Open block delimiters should be surrounded by empty lines',
+      recommendation: '--> add an empty line before line 10',
+      fixable: true,
+    });
+  });
+
+  it('lints opening terminator without proper spacing after', () => {
+    const adoc = stripIndent(`
+      Foo bar.
+
+      [.embedded-content-document.letter]
+      --
+      Foo bar
+      and baz.
+
+      --
+
+      Foo
+    `).trim();
+
+    const results = openBlocks(adoc);
+    expect(results).toHaveLength(1);
+    expect(results[0]).toEqual({
+      line: 5,
+      column: false,
+      type: 'error',
+      rule: 'open-block',
+      message: 'Open block delimiters should be surrounded by empty lines',
+      recommendation: '--> add an empty line before line 5',
+      fixable: true,
+    });
+  });
+});
+
 describe('openBlocks() - unlabeled-open-block-delimiter', () => {
   it('lints the opening of a block with no class label', () => {
     const adoc = stripIndent(`
@@ -21,7 +104,8 @@ describe('openBlocks() - unlabeled-open-block-delimiter', () => {
       column: false,
       type: 'error',
       rule: 'open-block',
-      message: 'Open blocks must be started with a class designation, like `[.embedded-content-document.letter]`',
+      message:
+        'Open blocks must be started with a class designation, like `[.embedded-content-document.letter]`',
     });
   });
 
@@ -52,7 +136,6 @@ describe('openBlocks() - unlabeled-open-block-delimiter', () => {
     expect(results[0].message).toMatch(/must be started/);
   });
 });
-
 
 describe('openBlocks() - unterminated-open-block', () => {
   it('lints an unterminated open block', () => {
