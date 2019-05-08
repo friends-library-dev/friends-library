f5e7d3826bce776916b14166f009e2b4a3c76d1c
Jared Henderson <jared@netrivet.com>
2019-05-08 16:13:22 -0400

    cover: faux css tagged template literal fn for vscode goodness

--- packages/cover/src/components/Cover/css/guides.css.ts
+++ packages/cover/src/components/Cover/css/guides.css.ts
@@ -1,72 +1,73 @@
-const css: string = /* css */ `
+import { css } from '../css';
 
-.guide {
-  box-sizing: border-box;
-  display: var(--guidesDisplay);
-  border-style: dashed;
-  border-color: red;
-  position: absolute;
-  border-width: 0;
-}
+const guidesCss: string = css`
+  .guide {
+    box-sizing: border-box;
+    display: var(--guidesDisplay);
+    border-style: dashed;
+    border-color: red;
+    position: absolute;
+    border-width: 0;
+  }
 
-.guide--vertical {
-  border-right-width: 1px;
-  height: 100%;
-}
+  .guide--vertical {
+    border-right-width: 1px;
+    height: 100%;
+  }
 
-.guide--horizontal {
-  width: 100%;
-}
+  .guide--horizontal {
+    width: 100%;
+  }
 
-.guide--box {
-  border-width:1px;
-}
+  .guide--box {
+    border-width: 1px;
+  }
 
-.guide--trim-bleed{
-  height: var(--pageHeight);
-  width: var(--guideSafetyWidth);
-  left: var(--trimBleed);
-  top: var(--trimBleed);
-}
+  .guide--trim-bleed {
+    height: var(--pageHeight);
+    width: var(--guideSafetyWidth);
+    left: var(--trimBleed);
+    top: var(--trimBleed);
+  }
 
-.guide--spine {
-  border-color: blue;
-}
+  .guide--spine {
+    border-color: blue;
+  }
 
-.guide--spine-right {
-  right: var(--edgeToSpine);
-}
+  .guide--spine-right {
+    right: var(--edgeToSpine);
+  }
 
-.guide--spine-left {
-  left: var(--edgeToSpine);
-}
+  .guide--spine-left {
+    left: var(--edgeToSpine);
+  }
 
-.guide--spine-center {
-  border-color: magenta;
-  opacity: 0.75;
-  left: var(--halfSpineWidth);
-}
+  .guide--spine-center {
+    border-color: magenta;
+    opacity: 0.75;
+    left: var(--halfSpineWidth);
+  }
 
-.guide--safety {
-  top: var(--edgeToSafe);
-  border-color: orange;
-  width: var(--safeAreaWidth);
-  height: var(--safeAreaHeight);
-}
+  .guide--safety {
+    top: var(--edgeToSafe);
+    border-color: orange;
+    width: var(--safeAreaWidth);
+    height: var(--safeAreaHeight);
+  }
 
-.guide--safety-front {
-  right: var(--edgeToSafe);
-}
+  .guide--safety-front {
+    right: var(--edgeToSafe);
+  }
 
-.guide--safety-back {
-  left: var(--edgeToSafe);
-}
+  .guide--safety-back {
+    left: var(--edgeToSafe);
+  }
 
-.cover--show-guides .front__safe,
-.cover--show-guides .back__safe {
-  outline: 1px dashed orange;
-  outline-offset: -1px;
-}
+  .cover--show-guides .front__safe,
+  .cover--show-guides .back__safe {
+    outline: 1px dashed orange;
+    outline-offset: -1px;
+  }
 `;
 
-export default css;
+export default guidesCss;
