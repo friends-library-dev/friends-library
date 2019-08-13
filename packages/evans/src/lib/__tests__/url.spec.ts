e334ebdd11635f38e31b293d6e2ffd604cbdb53b
Jared Henderson <jared@netrivet.com>
2019-08-13 08:45:20 -0400

    fix broken test

--- packages/evans/src/lib/__tests__/url.spec.ts
+++ packages/evans/src/lib/__tests__/url.spec.ts
@@ -1,14 +1,21 @@
-import { getFriend } from '@friends-library/friends';
+import { Friend, Document, Edition, Format } from '@friends-library/friends';
 import { formatUrl } from '../url';
 
-const friend = getFriend('george-fox', 'en');
-const doc = friend.documents[0];
-const edition = doc.editions[0];
-const format = edition.formats[0];
+const friend = new Friend('fr-id', 'en', 'George Fox', 'george-fox');
+const doc = new Document('doc-id', '', '', 'journal', '', 'Journal_of_George_Fox');
+const edition = new Edition('original');
+const format = new Format('pdf');
+friend.documents = [doc];
+doc.editions = [edition];
+edition.formats = [format];
+format.edition = edition;
+edition.document = doc;
+doc.friend = friend;
 
 describe('formatUrl()', () => {
   it('should return a correctly formed function log url', () => {
-    const expected = `/.netlify/functions/site/download/web/${doc.id}/en/george-fox/journal/original/pdf-web/Journal_of_George_Fox--original.pdf`;
+    const expected =
+      '/.netlify/functions/site/download/web/doc-id/en/george-fox/journal/original/pdf-web/Journal_of_George_Fox--original.pdf';
     expect(formatUrl(format)).toBe(expected);
   });
 });
