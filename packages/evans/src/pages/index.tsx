c30c349a0f275ccc9882027c125912f1e5b9142f
Jared Henderson <jared@netrivet.com>
2019-08-14 16:33:09 -0400

    ui: very rough formats block

--- packages/evans/src/pages/index.tsx
+++ packages/evans/src/pages/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/Layout';
-import TempBlock from '../components/TempBlock';
 import {
   WhoWereTheQuakers,
   Hero,
@@ -9,6 +7,7 @@ import {
   GettingStarted,
   ExploreBooks,
   FeaturedBooks,
+  Formats,
 } from '@friends-library/ui';
 
 export default () => (
@@ -18,23 +17,7 @@ export default () => (
     <FeaturedBooks />
     <GettingStarted />
     <WhoWereTheQuakers />
-    <TempBlock>
-      <h1>Formats and Editions</h1>
-      <p>
-        On this site you will find many books available in multiple formats. Our desire is
-        to make these precious writings as accessible as possible to the modern seeker
-        after truth. Therefore, each book has been converted to 3 digital formats:{' '}
-        <code>pdf</code>, <code>mobi</code> (for Kindle), and <code>epub</code> (all other
-        e-readers including iBooks). A growing number of our books also have a free audio
-        version, and some are also available in a paperback edition.
-      </p>
-      <p>
-        In addition to the various formats of the book, we also offer minimally and
-        carefully modernized versions of each book, because most modern readers find some
-        of the antiquated grammer and vocabulary to be a stumbling block to comprehension.
-        For more info on modernization, <Link to="/modernization">click here</Link>.
-      </p>
-    </TempBlock>
+    <Formats />
     <ExploreBooks />
   </Layout>
 );
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -9,6 +9,7 @@ export { default as SubHero } from './blocks/SubHero';
 export { default as ExploreBooks } from './blocks/ExploreBooks';
 export { default as GettingStarted } from './blocks/GettingStarted';
 export { default as FeaturedBooks } from './blocks/FeaturedBooks';
+export { default as Formats } from './blocks/Formats';
 export { t, useLocale } from './translation';
 export { enTheme, esTheme };
 export type Theme = typeof enTheme;
