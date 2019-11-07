d57adf94e45cc9d59e301a82bffc18fdfb931c31
Jared Henderson <jared@netrivet.com>
2019-11-07 13:56:07 -0500

    use new convention for custom code file names

--- packages/cli/src/fs-precursor/hydrate/custom-code.ts
+++ packages/cli/src/fs-precursor/hydrate/custom-code.ts
@@ -11,8 +11,7 @@ export default function customCode(dpc: FsDocPrecursor): void {
     const type = path.extname(file).replace(/^\./, '') as 'css' | 'html';
     const target = path
       .basename(file)
-      .replace(/\.(css|html)$/, '')
-      .replace(/^cover$/, 'paperback-cover') as keyof DocPrecursor['customCode']['css'];
+      .replace(/\.(css|html)$/, '') as keyof DocPrecursor['customCode']['css'];
     const content = fs.readFileSync(file).toString();
     dpc.customCode[type][target] = content;
   });
