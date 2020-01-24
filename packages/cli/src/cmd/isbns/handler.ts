485eed7f73817b1bd026a801eb672fb5a07ad5f3
Jared Henderson <jared@netrivet.com>
2020-01-24 14:31:51 -0500

    cli: isbn make csv command

--- packages/cli/src/cmd/isbns/handler.ts
+++ packages/cli/src/cmd/isbns/handler.ts
@@ -1,19 +1,57 @@
 import fs from 'fs';
-import { spawn } from 'child_process';
+import { spawn, execSync } from 'child_process';
 import { c, log, red } from '@friends-library/cli-utils/color';
 import { ISBN } from '@friends-library/types';
 import { getAllFriends } from '@friends-library/friends';
+import isbns from './isbns.json';
 
-interface Argv {
-  next: boolean;
-}
+const flags = ['next', 'makeImages', 'makeCsv'] as const;
 
-export default function handler({ next }: Argv): void {
-  if (!next) {
-    red('Currently `--next` is the only supported option');
+type Argv = { [k in typeof flags[number]]: boolean };
+
+export default function handler(argv: Argv): void {
+  if (!flags.some(f => argv[f])) {
+    const optionList = flags.map(optionify).join(' ');
+    log(c`\n{red Supply at least one option:} {green ${optionList}}\n`);
     process.exit(1);
   }
 
+  argv.next && next();
+  argv.makeImages && makeImgs();
+  argv.makeCsv && makeCsv();
+}
+
+function makeCsv(): void {
+  const editions = [...getAllFriends('en', true), ...getAllFriends('es', true)]
+    .flatMap(friend => friend.documents)
+    .flatMap(document => document.editions);
+
+  const rows: [string, string, string, string, string, string][] = [
+    ['ISBN', 'Title', 'Author Last', 'Author First', 'Editor Last', 'Editor First'],
+  ];
+
+  editions.forEach(edition => {
+    const document = edition.document;
+    const [authorFirst, authorLast = ''] = splitName(document.friend.name);
+    const [editorFirst, editorLast = ''] = splitName(edition.editor || '');
+    rows.push([
+      edition.isbn,
+      edition.type === 'original'
+        ? document.title
+        : `${document.title} (${edition.type})`,
+      authorLast,
+      authorFirst === 'Compilations' ? '' : authorFirst,
+      editorLast,
+      editorFirst,
+    ]);
+  });
+
+  console.table(rows);
+  fs.writeFileSync(`${__dirname}/isbns.csv`, toCsvString(rows));
+  log(c`\nCSV file written at: {magenta ${__dirname}/isbns.csv}\n`);
+}
+
+function next(): void {
   const used: ISBN[] = [];
   [...getAllFriends('en', true), ...getAllFriends('es', true)].forEach(friend => {
     friend.documents.forEach(doc => {
@@ -21,15 +59,7 @@ export default function handler({ next }: Argv): void {
     });
   });
 
-  const all: ISBN[] = fs
-    .readFileSync(
-      `${process.cwd()}/packages/cover-web-app/public/images/isbn/_suffixes.txt`,
-    )
-    .toString()
-    .split('\n')
-    .map(suffix => `978-1-64476-${suffix}`);
-
-  for (let isbn of all) {
+  for (let isbn of isbns) {
     if (!used.includes(isbn)) {
       const pbcopy = spawn('pbcopy');
       pbcopy.stdin.write(isbn);
@@ -40,4 +70,34 @@ export default function handler({ next }: Argv): void {
   }
 
   red('All ISBNs used!');
+}
+
+function makeImgs(): void {
+  const url = 'http://bwipjs-api.metafloor.com/?bcid=isbn&includetext&guardwhitespace';
+  const imgDir = `${process.cwd()}/packages/cover-web-app/public/images/isbn/`;
+  isbns.forEach(isbn => {
+    execSync(`curl -Ss "${url}&text=${isbn}&scale=3&height=16" > ${imgDir}/${isbn}.png`);
+  });
+}
+
+function toCsvString(rows: string[][]): string {
+  return rows
+    .map(row => row.map(col => (col.includes(',') ? `"${col}"` : col)).join(','))
+    .join('\n');
+}
+
+function optionify(str: string): string {
+  return `--${str.replace(/([A-Z])/g, '-$1').toLowerCase()}`;
+}
+
+function splitName(name: string): string[] {
+  return name
+    .split('')
+    .reverse()
+    .join('')
+    .replace(' ', '*')
+    .split('')
+    .reverse()
+    .join('')
+    .split('*');
 }
--- packages/cli/src/cmd/isbns/index.ts
+++ packages/cli/src/cmd/isbns/index.ts
@@ -5,11 +5,22 @@ export const command = 'isbns';
 export const describe = 'interact with our block of isbns';
 
 export const builder: CommandBuilder = function(yargs) {
-  return yargs.option('next', {
-    type: 'boolean',
-    describe: 'print out the next unnassigned ISBN for usage',
-    default: false,
-  });
+  return yargs
+    .option('next', {
+      type: 'boolean',
+      describe: 'print out the next unnassigned ISBN for usage',
+      default: false,
+    })
+    .option('make-csv', {
+      type: 'boolean',
+      describe: 'make a .csv file for volunteer to use to enter info into bowker',
+      default: false,
+    })
+    .option('make-images', {
+      type: 'boolean',
+      describe: 'make png images for each isbn number',
+      default: false,
+    });
 };
 
 export { default as handler } from './handler';
