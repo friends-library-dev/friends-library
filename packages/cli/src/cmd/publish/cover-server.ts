77fb2386c5941341de507eddf63970586ea8057e
Jared Henderson <jared@netrivet.com>
2020-01-27 10:25:05 -0500

    add logo to audio images

--- packages/cli/src/cmd/publish/cover-server.ts
+++ packages/cli/src/cmd/publish/cover-server.ts
@@ -19,15 +19,8 @@ export function stop(port: number): void {
   execSync(`lsof -t -i tcp:${port} | xargs kill`);
 }
 
-export interface Clip {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-}
-
 export interface ScreenshotTaker {
-  (id: string, clip?: Clip): Promise<Buffer>;
+  (id: string, type: 'ebook' | 'audio'): Promise<Buffer>;
 }
 
 interface BrowserCloser {
@@ -43,12 +36,37 @@ export async function screenshot(
   await page.setViewport({ width: 1600, height: 2400 });
 
   return [
-    async (id: string, clip?: Clip): Promise<Buffer> => {
-      await page.goto(`http://localhost:${port}?capture=ebook&id=${id}`);
+    async (id: string, type: 'ebook' | 'audio'): Promise<Buffer> => {
+      const clip = type === 'ebook' ? false : getAudioImageClip();
+      await page.goto(`http://localhost:${port}?capture=${type}&id=${id}`);
       return page.screenshot({ encoding: 'binary', ...(clip ? { clip } : {}) });
     },
-    async () => {
-      await browser.close();
-    },
+    async () => await browser.close(),
   ];
+}
+
+function getAudioImageClip(): {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+} {
+  // height (in px) of top white bar, which we clip out
+  const TOP_WHITE_BAR_HEIGHT = 451;
+
+  // quasi-arbitrary value that tightens in on the main title square
+  const ZOOM = 200;
+
+  // a little extra room beyond the height of the top white bar, to center better
+  const Y_PADDING = ZOOM / 10;
+
+  // actual width (in px) of full ebook screenshot cover element
+  const FULL_WIDTH = 1600;
+
+  return {
+    x: 0 + ZOOM / 2,
+    y: TOP_WHITE_BAR_HEIGHT + Y_PADDING + ZOOM / 2,
+    width: FULL_WIDTH - ZOOM,
+    height: FULL_WIDTH - ZOOM,
+  };
 }
