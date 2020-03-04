d1fbf515daf15050528daabeb99d0cd96c754d77
Jared Henderson <jared@netrivet.com>
2020-03-04 16:41:13 -0500

    ui: usable english text for all friends page

--- packages/ui/src/pages/friends/CompilationsBlock.tsx
+++ packages/ui/src/pages/friends/CompilationsBlock.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'gatsby-link';
 import Button from '../../Button';
 import './CompilationsBlock.css';
 
@@ -6,12 +7,20 @@ const CompilationsBlock: React.FC = () => (
   <div className="CompilationsBlock text-center text-white px-16 py-24 md:py-24 xl:py-32">
     <h1 className="sans-wider text-4xl font-bold">Compilations</h1>
     <p className="body-text text-white py-8 text-lg leading-loose max-w-screen-sm mx-auto">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-      exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      We also have a number of books comprised of the writings of more than one author.
+      Some of these were compiled and published during the time of the early Friends, and
+      some were compiled recently by the editors of this website. Among the latter is{' '}
+      <Link
+        to="/compilations/truth-in-the-inward-parts"
+        className="subtle-link text-white"
+      >
+        Truth in the Inward Parts
+      </Link>
+      , a work containing extracts from the journals and letters of 10 early friends
+      describing their convincement and spiritual growth.
     </p>
     <Button to="/compilations" className="mt-4 mx-auto">
-      Learn More
+      View Compilations &rarr;
     </Button>
   </div>
 );
--- packages/ui/src/pages/friends/FriendsPageHero.tsx
+++ packages/ui/src/pages/friends/FriendsPageHero.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
+import { makeScroller } from '../../lib/scroll';
 import './FriendsPageHero.css';
 
-const FriendsPageHero: React.FC<{ numFriends: number }> = () => (
+const FriendsPageHero: React.FC<{ numFriends: number }> = ({ numFriends }) => (
   <div className="FriendsPageHero text-center text-white px-16 py-16 md:py-24 xl:py-32">
     <h1 className="sans-wider text-4xl font-bold">Authors</h1>
     <p className="body-text text-white py-8 text-lg leading-loose max-w-screen-sm mx-auto">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-      exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Friends Library currently contains books written by{' '}
+      <span className="font-bold">{numFriends}</span> early Friends, and more authors are
+      being regularly added. Check out our recently-added authors, or browse the full list
+      below. You can also{' '}
+      <a
+        href="#ControlsBlock"
+        className="underline"
+        onClick={e => {
+          e.preventDefault();
+          makeScroller('#ControlsBlock')();
+        }}
+      >
+        sort
+      </a>{' '}
+      and{' '}
+      <a
+        href="#ControlsBlock"
+        className="underline"
+        onClick={e => {
+          e.preventDefault();
+          makeScroller('#ControlsBlock')();
+        }}
+      >
+        search
+      </a>{' '}
+      to find exactly who you&rsquo;re looking for.
     </p>
   </div>
 );
