16c4c6d9ffc112a7bf2709da4b3abdd6143439ad
Jared Henderson <jared@netrivet.com>
2020-03-06 15:12:07 -0500

    cli: each-ymls helper cmd

---
+++ packages/cli/src/cmd/each-yml/index.ts
@@ -0,0 +1,6 @@
+export const command = 'each-yml';
+
+export const describe =
+  'playground helper cmd for programatically interacting with yml files';
+
+export { default as handler } from './handler';
