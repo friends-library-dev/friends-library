bcc510496d4bd51dd3a5824ea788ccd1eda01c5e
Jared Henderson <jared@netrivet.com>
2020-03-09 12:06:13 -0400

    ui: text tweaks for all friends page

--- packages/ui/src/pages/friends/CompilationsBlock.tsx
+++ packages/ui/src/pages/friends/CompilationsBlock.tsx
@@ -7,9 +7,10 @@ const CompilationsBlock: React.FC = () => (
   <div className="CompilationsBlock text-center text-white px-16 py-24 md:py-24 xl:py-32">
     <h1 className="sans-wider text-4xl font-bold">Compilations</h1>
     <p className="body-text text-white py-8 text-lg leading-loose max-w-screen-sm mx-auto">
-      We also have a number of books comprised of the writings of more than one author.
-      Some of these were compiled and published during the time of the early Friends, and
-      some were compiled recently by the editors of this website. Among the latter is{' '}
+      We also have books that contain a collection of writings from several different
+      authors. Some of these books were compiled and published by early Friends, and some
+      were compiled more recently by the editors of this website. Among the latter group
+      is the book{' '}
       <Link
         to="/compilations/truth-in-the-inward-parts"
         className="subtle-link text-white"
@@ -17,7 +18,7 @@ const CompilationsBlock: React.FC = () => (
         Truth in the Inward Parts
       </Link>
       , a work containing extracts from the journals and letters of 10 early friends
-      describing their convincement and spiritual growth.
+      describing their discovery of truth and spiritual growth.
     </p>
     <Button to="/compilations" className="mt-4 mx-auto">
       View Compilations &rarr;
