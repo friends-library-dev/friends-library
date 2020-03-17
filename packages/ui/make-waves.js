648ccc1a4a7a1fa263525b9c8d2b53becc87dce0
Jared Henderson <jared@netrivet.com>
2020-03-17 15:54:41 -0400

    ui: fix WaveBottomBlock so it works for very wide screens

---
+++ packages/ui/make-waves.js
@@ -0,0 +1,30 @@
+// @ts-check
+const fs = require('fs');
+const colors = require('./tailwind.config.js').theme.extend.colors;
+
+const svg = `
+<svg xmlns="http://www.w3.org/2000/svg" width="1800" height="350" viewBox="0 150 1800 490" preserveAspectRatio="none">
+  <path
+    fill="__COLOR__"
+    d="__POINTS__"
+  />
+</svg>
+`.trim();
+
+// NOTE: to tweak the wave SVG paths/shape, go to this codepen: https://codepen.io/jaredh159/pen/yLNEgeg
+// change stuff, and copy out the new point data. IMPORTANT: if you make changes to the SVG path that you
+// end up keeping, change the initial state of the main React component, use the "Click to log state"
+// button I added, and pull up the CodePen console to get a dump of the current state
+const points =
+  'M 0 270 Q 70 230 160 290 Q 235 340 295 260 Q 385 145 468 246 Q 530 320 597 267 Q 685 195 760 290 Q 820 360 880 295 Q 960 205 1045 275 Q 1105 325 1155 295 Q 1225 250 1295 300 Q 1355 340 1400 295 Q 1460 230 1545 270 Q 1635 310 1692 264 Q 1745 220 1800 235 L 1800 640 L 0 640 Z';
+
+Object.keys(colors)
+  .filter(c => c.match(/^fl(blue|gold|maroon|green)-\d00$/))
+  .forEach(name => {
+    const rgb = colors[name];
+    const svgPath = `${__dirname}/src/images/waves/${name.replace(/^fl/, '')}.svg`;
+    fs.writeFileSync(
+      svgPath,
+      svg.replace('__COLOR__', rgb).replace('__POINTS__', points),
+    );
+  });
--- packages/ui/src/blocks/WaveBottomBlock.tsx
+++ packages/ui/src/blocks/WaveBottomBlock.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import cx from 'classnames';
+import './WaveBottomBlock.css';
 
 interface Props {
   color: 'blue' | 'maroon' | 'green' | 'gold';
@@ -9,30 +10,13 @@ interface Props {
 
 const WaveBottomBlock: React.FC<Props> = ({ id, color, children, className }) => {
   return (
-    <section id={id} className={cx(className, 'overflow-hidden relative')}>
-      <Bump colorClass={`text-fl${color}-400`} viewBox="100 -180 1200 600" />
-      <Bump colorClass={`text-fl${color}-800`} viewBox="-0 -180 1010 600" />
-      <Bump colorClass={`text-fl${color}-600`} viewBox="300 -245 1020 600" />
-      <div className="z-10 relative">{children}</div>
+    <section
+      id={id}
+      className={cx(className, `WaveBottomBlock WaveBottomBlock--${color}`)}
+    >
+      {children}
     </section>
   );
 };
 
 export default WaveBottomBlock;
-
-const Bump: React.FC<{ colorClass: string; viewBox: string }> = ({
-  colorClass,
-  viewBox,
-}) => (
-  <svg
-    className="absolute bottom-0"
-    style={{ height: '60vh', width: '3000px', left: '-60%' }}
-    viewBox={viewBox}
-    preserveAspectRatio="none"
-  >
-    <path
-      className={`${colorClass} fill-current`}
-      d="M 30 300 Q 120 210 210 270 Q 315 330 420 240 Q 510 180 615 270 C 750 390 840 210 930 315 C 1005 405 1140 285 1200 255 L 1200 600 L 15 600 Z"
-    />
-  </svg>
-);
