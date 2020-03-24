4b50d6a31994a8c052b7287aad9f8f341b5e6c64
Jared Henderson <jared@netrivet.com>
2020-03-24 15:06:24 -0400

    fix spanish URLs

--- packages/evans/gatsby-node.js
+++ packages/evans/gatsby-node.js
@@ -3,6 +3,7 @@ require('ts-node').register();
 exports.sourceNodes = require('./src/build/source-nodes').default;
 exports.createPagesStatefully = require('./src/build/create-pages-statefully').default;
 exports.createPages = require('./src/build/create-pages').default;
+exports.onCreatePage = require('./src/build/on-create-page').default;
 exports.onCreateDevServer = require('./src/build/on-create-dev-server').default;
 exports.onPostBuild = require('./src/build/on-post-build').default;
 exports.createSchemaCustomization = require('./src/build/create-schema-customization').default;
