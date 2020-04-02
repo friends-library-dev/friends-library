8dc51f261bc8f026ad4458232c6fbe3133cb3bcd
Jared Henderson <jared@netrivet.com>
2020-04-02 11:22:19 -0400

    ui: prevent checkout modal from being partially inaccesible on short screens

--- packages/ui/src/checkout/Modal.tsx
+++ packages/ui/src/checkout/Modal.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
-import './Modal.css';
 
 interface Props {
   onClose: () => void;
@@ -8,11 +7,12 @@ interface Props {
 
 const Modal: React.FC<Props> = ({ children, onClose }) => (
   <DialogOverlay
+    style={{ zIndex: 9999 }}
     onDismiss={onClose}
-    className="CheckoutModal bg-white md:bg-transparent md:flex items-center justify-center inset-0 fixed overflow-auto"
+    className="CheckoutModal bg-white md:bg-transparent inset-0 fixed overflow-auto"
   >
     <DialogContent
-      className="CheckoutModal__Content relative w-full max-w-6xl p-10 md:p-16 outline-none bg-white md:shadow-direct"
+      className="relative w-full md:my-8 md:mx-auto max-w-6xl p-10 md:p-16 outline-none bg-white md:shadow-direct"
       aria-label="Cart"
     >
       <button
