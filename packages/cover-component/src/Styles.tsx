7eb043d5b286669362149f9845c25dbf73c8bc58
Jared Henderson <jared@netrivet.com>
2020-04-06 15:22:25 -0400

    cover-component: minify cover css

--- packages/cover-component/src/Styles.tsx
+++ packages/cover-component/src/Styles.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import * as css from './css';
 
 const CoverWebStylesAllStatic: React.FC = () => (
-  <style className="cover-styles">{`${css.allStatic()}`}</style>
+  <style className="cover-styles">{`${minifyCss(css.allStatic())}`}</style>
 );
 
 const CoverWebStylesAllDynamic: React.FC<{ scaler?: number; scope?: string }> = ({
   scaler,
   scope,
-}) => <style>{`${css.allDynamic(scaler, scope)}`}</style>;
+}) => <style>{`${minifyCss(css.allDynamic(scaler, scope))}`}</style>;
 
 const CoverWebStylesSizes: React.FC = () => (
   <>
@@ -18,13 +18,24 @@ const CoverWebStylesSizes: React.FC = () => (
     <CoverWebStylesAllDynamic scaler={1 / 2} scope="1-2" />
     <CoverWebStylesAllDynamic scaler={1 / 3} scope="1-3" />
     <CoverWebStylesAllDynamic scaler={1 / 4} scope="1-4" />
-    <style className="cover-styles">{`
+    <style className="cover-styles">
+      {minifyCss(`
       .Cover.Cover--3d.Cover--scope-3-5 { perspective: 1200px; }
       .Cover.Cover--3d.Cover--scope-1-2 { perspective: 1000px; }
       .Cover.Cover--3d.Cover--scope-1-3 { perspective: 800px; }
       .Cover.Cover--3d.Cover--scope-1-4 { perspective: 600px; }
-    `}</style>
+    `)}
+    </style>
   </>
 );
 
 export { CoverWebStylesAllStatic, CoverWebStylesAllDynamic, CoverWebStylesSizes };
+
+function minifyCss(css: string): string {
+  return css
+    .replace(/\/\*(?:(?!\*\/)[\s\S])*\*\/|[\r\n\t]+/g, '')
+    .replace(/ {2,}/g, ' ')
+    .replace(/ ([{:}]) /g, '$1')
+    .replace(/([;,]) /g, '$1')
+    .replace(/ !/g, '!');
+}
