b3efc543ebd7530b407dad3789ac154d071812d9
Jared Henderson <jared@netrivet.com>
2020-04-09 16:21:08 -0400

    evans: remove unused ttag dep

--- packages/evans/babel.config.js
+++ packages/evans/babel.config.js
@@ -1,13 +1,3 @@
 module.exports = {
   presets: ['babel-preset-gatsby'],
-  plugins: [
-    [
-      'babel-plugin-ttag',
-      {
-        resolve: {
-          translations: process.env.GATSBY_LANG === 'es' ? 'es.po' : 'default',
-        },
-      },
-    ],
-  ],
 };
