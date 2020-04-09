52f6f316788c75e79abe88b9270c7a1b3de21ef2
Jared Henderson <jared@netrivet.com>
2020-04-09 16:40:19 -0400

    evans: ensure all urls un-trailing-slashed

--- packages/evans/gatsby-config.js
+++ packages/evans/gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-remove-trailing-slashes',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
