66623af300e6691a1207d74343ff8b73cea9f074
Jason Henderson <henderjay@gmail.com>
2020-04-22 15:49:21 -0400

    update turford and spalding blurbs

--- packages/evans/src/components/Layout.tsx
+++ packages/evans/src/components/Layout.tsx
@@ -17,7 +17,7 @@ import './Layout.css';
 const store = CartStore.getSingleton();
 
 const Layout: React.FC = ({ children }) => {
-  // force netlify deploy #494
+  // force netlify deploy #495
   const [numCartItems] = useNumCartItems();
   const [jsEnabled, setJsEnabled] = useState<boolean>(false);
   const [webp, setWebp] = useState<boolean | null>(null);
