616ca78475e6e117fd534fb560968177c9577668
Jared Henderson <jared@netrivet.com>
2020-04-24 16:35:54 -0400

    ui: make search pagination links tabbable

--- packages/ui/src/algolia/Pagination.tsx
+++ packages/ui/src/algolia/Pagination.tsx
@@ -7,9 +7,15 @@ interface Props {
   currentRefinement: number;
   nbPages: number;
   refine: (page: number) => any;
+  createURL: (page: number) => any;
 }
 
-const Pagination: React.FC<Props> = ({ currentRefinement, nbPages, refine }) => {
+const Pagination: React.FC<Props> = ({
+  currentRefinement,
+  createURL,
+  nbPages,
+  refine,
+}) => {
   if (nbPages < 2) return null;
   return (
     <ul className="flex justify-center items-center mt-2">
@@ -20,18 +26,23 @@ const Pagination: React.FC<Props> = ({ currentRefinement, nbPages, refine }) => 
         const page = index + 1;
         const isActive = currentRefinement === page;
         return (
-          <li
-            onClick={() => refine(page)}
-            key={`page-${page}`}
-            className={cx(
-              'flex items-center justify-center',
-              'w-5 h-5 rounded-md ml-1',
-              'text-xs cursor-pointer',
-              isActive && 'bg-flprimary-400 text-white',
-              !isActive && 'bg-flgray-400 text-flgray-500',
-            )}
-          >
-            <span>{page}</span>
+          <li key={`page-${page}`}>
+            <a
+              className={cx(
+                'flex items-center justify-center',
+                'w-5 h-5 rounded-md ml-1',
+                'text-xs cursor-pointer subtle-focus',
+                isActive && 'bg-flprimary-400 text-white',
+                !isActive && 'bg-flgray-400 text-flgray-500',
+              )}
+              href={createURL(page)}
+              onClick={e => {
+                e.preventDefault();
+                refine(page);
+              }}
+            >
+              {page}
+            </a>
           </li>
         );
       })}
