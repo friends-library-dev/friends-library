ac327a77a6374db6cea07c379d3b793c1e4aea1c
Jared Henderson <jared@netrivet.com>
2020-05-07 15:22:12 -0400

    refine deployment action

--- packages/evans/deploy.js
+++ packages/evans/deploy.js
@@ -7,16 +7,23 @@ async function main() {
   const { SHORT_SHA, PR_NUMBER, PR_TITLE, DEPLOY_SITE_ID, GITHUB_REF } = process.env;
   const isOpenPr = GITHUB_REF !== 'refs/heads/master';
 
+  let message = `Push commit @${SHORT_SHA}`;
+  if (PR_NUMBER && isOpenPr) {
+    message = `PR #${PR_NUMBER}@${SHORT_SHA} "${PR_TITLE}"`;
+  } else if (PR_NUMBER && !isOpenPr) {
+    message = `Merge PR#${PR_NUMBER}@${SHORT_SHA} to master`;
+  }
+
   try {
     const res = await client.deploy(DEPLOY_SITE_ID, `./public`, {
+      message,
       fnDir: `../functions/dist`,
       draft: isOpenPr,
-      message: isOpenPr
-        ? `PR #${PR_NUMBER}@${SHORT_SHA} "${PR_TITLE}"`
-        : `Merge PR#${PR_NUMBER}@${SHORT_SHA} to master`,
       statusCb: status => status.type !== 'hashing' && console.log(status.msg),
     });
-    core.setOutput('url', res.deploy.deploy_ssl_url);
+    const url = res.deploy.deploy_ssl_url;
+    core.setOutput('url', url);
+    console.log(`Deploy url: ${url}`);
   } catch (error) {
     core.setFailed(error.message);
   }
