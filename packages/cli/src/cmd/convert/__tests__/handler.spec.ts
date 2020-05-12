dc9ade1bf766400aa1ad666f9b8e7d9b41e410d1
Jared Henderson <jared@netrivet.com>
2020-05-12 13:07:18 -0400

    cli: fix docbook footnote empty paragraph conversion

--- packages/cli/src/cmd/convert/__tests__/handler.spec.ts
+++ packages/cli/src/cmd/convert/__tests__/handler.spec.ts
@@ -22,4 +22,23 @@ describe('convertHandler()', () => {
 
     fs.unlinkSync(adoc);
   });
+
+  it('correctly handles self-closing empty footnote paragraph <para/>', () => {
+    if (process.env.CI || spawnSync('docker', ['--version']).status !== 0) {
+      expect(true).toBe(true);
+      return;
+    }
+
+    const file = path.resolve(__dirname, './footnote.xml');
+    const origContents = fs.readFileSync(file).toString();
+    const adoc = file.replace(/\.xml$/, '.adoc');
+    fs.existsSync(adoc) && fs.unlinkSync(adoc);
+    convertHandler({ file, skipRefs: false });
+
+    expect(fs.existsSync(adoc)).toBe(true);
+    expect(fs.readFileSync(adoc).toString()).toContain('After empty p.');
+
+    fs.writeFileSync(file, origContents);
+    fs.unlinkSync(adoc);
+  });
 });
