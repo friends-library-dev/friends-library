5bd22ad814bba4bdf165ce2587ea4ea128ad921f
Jared Henderson <jared@netrivet.com>
2020-05-16 06:17:47 -0400

    locale: add missing translation for Taxes

--- packages/locale/src/strings.ts
+++ packages/locale/src/strings.ts
@@ -167,6 +167,7 @@ const strings: { [k: string]: string } = {
   'Select a Country': 'Selecciona tu país',
   Subtotal: 'Subtotal',
   Shipping: 'Envío',
+  Taxes: 'Impuestos',
   'Credit Card Fee Offset': 'Tarifa de Tarjeta de Crédito',
   'Grand Total': 'Total General',
   'Enter credit card details': 'Introduce la información de tu tarjeta de Crédito',
