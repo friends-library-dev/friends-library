bee6bc63545441ac0195889365cc26cdba06a6c2
Jared Henderson <jared@netrivet.com>
2020-05-22 15:53:05 -0400

    adoc-utils: handle unusual nested quote order, fixes #420

---
+++ packages/adoc-utils/src/__tests__/fragment.spec.ts
@@ -0,0 +1,27 @@
+import { backtickQuotesToEntities } from '../fragment';
+import { ADOC_SYNTAX as a, HTML_DEC_ENTITIES as h } from '@friends-library/types';
+
+describe('backTickQuotesToEntities()', () => {
+  const cases = [
+    [
+      `Foo ${a.LEFT_DOUBLE_QUOTE}bar${a.RIGHT_DOUBLE_QUOTE}${a.RIGHT_SINGLE_QUOTE}`,
+      `Foo ${h.LEFT_DOUBLE_QUOTE}bar${h.RIGHT_DOUBLE_QUOTE}${h.RIGHT_SINGLE_QUOTE}`,
+    ],
+    [
+      `Foo ${a.LEFT_SINGLE_QUOTE}${a.LEFT_DOUBLE_QUOTE}bar${a.RIGHT_DOUBLE_QUOTE}${a.RIGHT_SINGLE_QUOTE}`,
+      `Foo ${h.LEFT_SINGLE_QUOTE}${h.LEFT_DOUBLE_QUOTE}bar${h.RIGHT_DOUBLE_QUOTE}${h.RIGHT_SINGLE_QUOTE}`,
+    ],
+    [
+      `Foo ${a.LEFT_SINGLE_QUOTE}bar${a.RIGHT_SINGLE_QUOTE}${a.RIGHT_DOUBLE_QUOTE}`,
+      `Foo ${h.LEFT_SINGLE_QUOTE}bar${h.RIGHT_SINGLE_QUOTE}${h.RIGHT_DOUBLE_QUOTE}`,
+    ],
+    [
+      `Foo ${a.LEFT_DOUBLE_QUOTE}${a.LEFT_SINGLE_QUOTE}bar${a.RIGHT_SINGLE_QUOTE}${a.RIGHT_DOUBLE_QUOTE}`,
+      `Foo ${h.LEFT_DOUBLE_QUOTE}${h.LEFT_SINGLE_QUOTE}bar${h.RIGHT_SINGLE_QUOTE}${h.RIGHT_DOUBLE_QUOTE}`,
+    ],
+  ];
+
+  test.each(cases)('%s should become %s', (adoc, html) => {
+    expect(backtickQuotesToEntities(adoc)).toBe(html);
+  });
+});
