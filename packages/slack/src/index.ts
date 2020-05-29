271116fa855c21b89bb8ab866803754ec19e9701
Jared Henderson <jared@netrivet.com>
2020-05-29 17:14:29 -0400

    slack: disable slack temporarily

--- packages/slack/src/index.ts
+++ packages/slack/src/index.ts
@@ -1,19 +1,20 @@
-import env from '@friends-library/env';
-import { WebClient } from '@slack/client';
+// import env from '@friends-library/env';
+// import { WebClient } from '@slack/client';
 
 export async function send(
   msg: string,
   channel: string,
   emoji = ':robot_face:',
 ): Promise<void> {
-  getClient().chat.postMessage({
-    username: 'FL Bot',
-    icon_emoji: emoji,
-    parse: 'full',
-    channel,
-    text: msg,
-    blocks: [sectionBlock(msg)],
-  });
+  console.log({ msg, channel, emoji });
+  // getClient().chat.postMessage({
+  //   username: 'FL Bot',
+  //   icon_emoji: emoji,
+  //   parse: 'full',
+  //   channel,
+  //   text: msg,
+  //   blocks: [sectionBlock(msg)],
+  // });
 }
 
 export async function sendJson(
@@ -22,47 +23,46 @@ export async function sendJson(
   channel: string,
   emoji = ':robot_face:',
 ): Promise<void> {
-  const blocks = [sectionBlock(msg)];
-
-  for (const label in data) {
-    blocks.push(sectionBlock(`_${label.toUpperCase()}:_`));
-    blocks.push(sectionBlock('```' + JSON.stringify(data[label], null, 2) + '```'));
-  }
-
-  getClient().chat.postMessage({
-    username: 'FL Bot',
-    icon_emoji: emoji,
-    parse: 'full',
-    channel,
-    text: msg,
-    blocks,
-  });
+  console.log({ msg, channel, emoji, data });
+  // const blocks = [sectionBlock(msg)];
+  // for (const label in data) {
+  //   blocks.push(sectionBlock(`_${label.toUpperCase()}:_`));
+  //   blocks.push(sectionBlock('```' + JSON.stringify(data[label], null, 2) + '```'));
+  // }
+  // getClient().chat.postMessage({
+  //   username: 'FL Bot',
+  //   icon_emoji: emoji,
+  //   parse: 'full',
+  //   channel,
+  //   text: msg,
+  //   blocks,
+  // });
 }
 
-let client: WebClient | undefined;
+// let client: WebClient | undefined;
 
-function getClient(): WebClient {
-  if (!client) {
-    const { SLACK_API_TOKEN } = env.require('SLACK_API_TOKEN');
-    client = new WebClient(SLACK_API_TOKEN);
-  }
-  return client;
-}
+// function getClient(): WebClient {
+//   if (!client) {
+//     const { SLACK_API_TOKEN } = env.require('SLACK_API_TOKEN');
+//     client = new WebClient(SLACK_API_TOKEN);
+//   }
+//   return client;
+// }
 
-interface SectionBlock {
-  type: 'section';
-  text: {
-    type: 'mrkdwn';
-    text: string;
-  };
-}
+// interface SectionBlock {
+//   type: 'section';
+//   text: {
+//     type: 'mrkdwn';
+//     text: string;
+//   };
+// }
 
-function sectionBlock(text: string): SectionBlock {
-  return {
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text,
-    },
-  };
-}
+// function sectionBlock(text: string): SectionBlock {
+//   return {
+//     type: 'section',
+//     text: {
+//       type: 'mrkdwn',
+//       text,
+//     },
+//   };
+// }
