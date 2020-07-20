839dc8145e32a82367608d579eb3a2f569f84bfb
Jared Henderson <jared@netrivet.com>
2020-07-20 14:24:12 -0400

    adoc-convert: prefer hex entities for epubcheck compat

--- packages/adoc-convert/src/__tests__/utils.spec.ts
+++ packages/adoc-convert/src/__tests__/utils.spec.ts
@@ -2,7 +2,7 @@ import { htmlTitle, htmlShortTitle } from '../utils';
 
 describe(`htmlTitle`, () => {
   it(`should turn double-dash into emdash entity`, () => {
-    expect(htmlTitle(`Foo -- bar`)).toBe(`Foo &mdash; bar`);
+    expect(htmlTitle(`Foo -- bar`)).toBe(`Foo &#8212; bar`);
   });
 
   it(`should change trailing digits into roman numerals`, () => {
@@ -12,10 +12,10 @@ describe(`htmlTitle`, () => {
 
 describe(`htmlShortTitle`, () => {
   it(`should shorten volume to Vol.`, () => {
-    expect(htmlShortTitle(`Foo -- Volume 1`)).toBe(`Foo &mdash; Vol.&nbsp;I`);
+    expect(htmlShortTitle(`Foo -- Volume 1`)).toBe(`Foo &#8212; Vol.&#160;I`);
   });
 
   it(`should shorten spanish volumen to Vol.`, () => {
-    expect(htmlShortTitle(`Foo -- volumen 4`)).toBe(`Foo &mdash; Vol.&nbsp;IV`);
+    expect(htmlShortTitle(`Foo -- volumen 4`)).toBe(`Foo &#8212; Vol.&#160;IV`);
   });
 });
