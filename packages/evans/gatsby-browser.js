128caaaddca89c10af2e1ac02a3092c163b6e3f1
Jared Henderson <jared@netrivet.com>
2020-07-29 15:12:38 -0400

    evans: catch & log errors outside of react with window.onerror

--- packages/evans/gatsby-browser.js
+++ packages/evans/gatsby-browser.js
@@ -5,4 +5,26 @@ export function onClientEntry() {
   if (window.NodeList && !NodeList.prototype.forEach) {
     NodeList.prototype.forEach = Array.prototype.forEach;
   }
+
+  // catch uncaught errors
+  if (process.env.NODE_ENV !== `development`) {
+    window.onerror = (event, source, lineno, colno, err) => {
+      window.fetch(`/.netlify/functions/site/log-error`, {
+        method: `POST`,
+        headers: {
+          'Content-Type': `application/json`,
+        },
+        body: JSON.stringify({
+          error: err,
+          event: event,
+          source: source,
+          lineno: lineno,
+          colno: colno,
+          location: `window.onerror`,
+          url: window.location.href,
+          userAgent: navigator.userAgent,
+        }),
+      });
+    };
+  }
 }
