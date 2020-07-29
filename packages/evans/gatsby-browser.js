a54b05335b4c2bb7d078c31a84d5facd87b2fb1d
Jared Henderson <jared@netrivet.com>
2020-07-29 14:22:28 -0400

    evans: polyfill NodeList.forEach for IE and Opera

--- packages/evans/gatsby-browser.js
+++ packages/evans/gatsby-browser.js
@@ -1,1 +1,8 @@
 import '../ui/src/Tailwind.css';
+
+export function onClientEntry() {
+  // polyfill document.querySelectorAll().forEach support
+  if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+  }
+}
