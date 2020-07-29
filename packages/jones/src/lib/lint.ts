2b2a4b9dd4e9b5e83bc3701ef0e9b2ad7f86e533
Jared Henderson <jared@netrivet.com>
2020-07-29 13:41:17 -0400

    jones: only run a subset of lints while editing

--- packages/jones/src/lib/lint.ts
+++ packages/jones/src/lib/lint.ts
@@ -10,3 +10,32 @@ export function lintOptions(path: string): LintOptions {
   }
   return { lang: LANG, editionType };
 }
+
+export function editorSubsetLintOptions(path: string): LintOptions {
+  const options = lintOptions(path);
+  options.exclude = [
+    `am-pm`,
+    `capitalize`,
+    `consecutive-spaces`,
+    `consistent-name-spelling`,
+    `eof-newline`,
+    `git-conflict-markers`,
+    `initials-comma`,
+    `join-words`,
+    `leading-whitespace`,
+    `libre-office-artifacts`,
+    `mid-word-non-letter`,
+    `mid-word-uppercase`,
+    `modernize-words`,
+    `multiple-blank-lines`,
+    `no-undefined`,
+    `obsolete-spellings`,
+    `person-mismatch`,
+    `scan-errors`,
+    `title-length`,
+    `trailing-whitespace`,
+    `unexpected-period`,
+    `unhyphened-words`,
+  ];
+  return options;
+}
