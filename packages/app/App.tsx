8b24e4df087273a7f35f95ae1a221b0e4ba94d8d
Jared Henderson <jared@netrivet.com>
2020-08-01 19:17:51 -0400

    lol download

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
-import * as RNFS from 'react-native-fs';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, TouchableOpacity, Text } from 'react-native';
+import * as fs from 'react-native-fs';
+
+const MP3 = `https://flp-assets.nyc3.digitaloceanspaces.com/en/elizabeth-webb/letter/updated/Letter_of_Elizabeth_Webb--lq.mp3`;
+const LOCAL = `${fs.DocumentDirectoryPath}/webb.mp3`;
 
 const App: React.FC = () => {
+  const [downloaded, setDownloaded] = useState<boolean>(false);
+  const [downloading, setDownloading] = useState<boolean>(false);
+
+  useEffect(() => {
+    console.log('effect');
+    fs.exists(LOCAL).then((exists) => {
+      console.log({ exists });
+      if (exists) setDownloaded(true);
+    });
+  }, []);
+
   return (
-    <SafeAreaView>
-      <Text>{RNFS.DocumentDirectoryPath}</Text>
+    <SafeAreaView style={{ margin: 100 }}>
+      {!downloaded && !downloading && (
+        <TouchableOpacity
+          onPress={async () => {
+            setDownloading(true);
+            const dl = fs.downloadFile({ fromUrl: MP3, toFile: LOCAL });
+            const result = await dl.promise;
+            setDownloading(false);
+            setDownloaded(true);
+          }}>
+          <Text>Download File</Text>
+        </TouchableOpacity>
+      )}
+      {downloading && <Text>Downloading...</Text>}
+      {downloaded && (
+        <TouchableOpacity>
+          <Text>Play webb</Text>
+        </TouchableOpacity>
+      )}
     </SafeAreaView>
   );
 };
