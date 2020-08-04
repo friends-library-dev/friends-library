b48e39ba8cdba567985b46bd6748fb73576cb9a8
Jared Henderson <jared@netrivet.com>
2020-08-04 12:02:59 -0400

    WIP: audio list screen

--- packages/app/components/AudioListItem.tsx
+++ packages/app/components/AudioListItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Platform } from 'react-native';
+import tw from 'tailwind-rn';
 
 interface Props {
   title: string;
@@ -9,10 +10,39 @@ interface Props {
 
 const AudioListItem: React.FC<Props> = ({ title, friend, artworkUrl }) => {
   return (
-    <View>
-      <Image source={{ uri: artworkUrl, width: 50, height: 50 }} />
-      <Text>Title: {title}</Text>
-      <Text>Author: {friend}</Text>
+    <View style={tw(`flex-row p-2 border-b border-gray-500`)}>
+      <Image source={{ uri: artworkUrl, width: 90, height: 90 }} />
+      <View style={tw(`flex-col m-2 flex-shrink`)}>
+        <Text
+          style={{
+            ...tw(`text-xl`),
+            ...Platform.select({
+              ios: {
+                fontFamily: 'Baskerville',
+              },
+              android: {
+                fontFamily: 'serif',
+              },
+            }),
+          }}>
+          {title}
+        </Text>
+        <Text
+          style={{
+            ...tw(`mt-1`),
+            letterSpacing: 1,
+            ...Platform.select({
+              ios: {
+                fontFamily: 'HelveticaNeue-Light',
+              },
+              android: {
+                fontFamily: 'sans-serif',
+              },
+            }),
+          }}>
+          {friend}
+        </Text>
+      </View>
     </View>
   );
 };
