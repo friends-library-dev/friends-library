b405108a8354cec3e7645234d4a09e1ded3c1040
Jared Henderson <jared@netrivet.com>
2020-08-17 14:05:05 -0400

    remove notice about COVID shipping delays

--- packages/ui/src/checkout/Confirmation.tsx
+++ packages/ui/src/checkout/Confirmation.tsx
@@ -12,18 +12,6 @@ const Confirmation: React.FC<{ onClose: () => void; email: string }> = ({
   <div className="md:py-12">
     <Header>{t`Confirmed!`}</Header>
     <Progress step="Confirmation" />
-    <Dual.P className="bg-orange-600 font-sans text-white py-4 px-8 mt-10 -mb-2">
-      <>
-        Note: Due to complications with shipping having to do with the ongoing COVID-19
-        pandemic, your book order may arrive as much as 10-15 days later than normal.
-        We’re sorry for the inconvenience.
-      </>
-      <>
-        Nota: Debido a las complicaciones que tienen los envíos por la pandemia del
-        COVID-19, tu pedido de libros puede demorar en llegar hasta 10-15 días más de lo
-        normal. Lamentamos las molestias que esto pueda ocasionarte.
-      </>
-    </Dual.P>
     <Dual.P className="body-text text-center mt-10">
       <>
         We’ve processed your order. You will receive an email to <code>{email}</code> with
