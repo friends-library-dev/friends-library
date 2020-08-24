fcffa30eb8fbbbacbf12cb5de095a08b4de75f74
Jared Henderson <jared@netrivet.com>
2020-08-24 14:27:40 -0400

    friends: prefer slug 'life-and-letters' over 'life-letters'

--- packages/ui/stories/getting-started.stories.tsx
+++ packages/ui/stories/getting-started.stories.tsx
@@ -37,7 +37,7 @@ const books = [
     title: `Life and Letters of Catherine Payton`,
     author: `Catherine Payton`,
     edition: `modernized` as const,
-    documentUrl: `/catherine-payton/life-letters`,
+    documentUrl: `/catherine-payton/life-and-letters`,
     authorUrl: `/`,
     hasAudio: false,
   },
