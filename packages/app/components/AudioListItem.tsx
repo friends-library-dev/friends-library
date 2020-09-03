c0ff5c945776dcf6a09281a2befded279d077114
Jared Henderson <jared@netrivet.com>
2020-09-03 11:10:03 -0400

    WIP: rad tweaks

--- packages/app/components/AudioListItem.tsx
+++ packages/app/components/AudioListItem.tsx
@@ -16,7 +16,7 @@ const AudioListItem: React.FC<Props> = ({ id, title, friend }) => {
     <View style={tw(`flex-row p-2 border-b border-gray-500`)}>
       <Artwork id={id} size={90} />
       <View style={tw(`flex-col m-2 flex-shrink`)}>
-        <Serif size={22} style={tw(`pb-1`)}>
+        <Serif size={22} style={tw(`pb-1`)} numberOfLines={2}>
           {shortTitle(title)}
         </Serif>
         {!friend.startsWith(`Compila`) && <Sans size={16}>{friend}</Sans>}
--- packages/app/lib/utils.ts
+++ packages/app/lib/utils.ts
@@ -3,6 +3,6 @@ import { htmlShortTitle } from '@friends-library/adoc-convert';
 
 export function shortTitle(title: string): string {
   return htmlShortTitle(title)
-    .replace(new RegExp(HEX.MDASH, 'g'), '—')
+    .replace(new RegExp(HEX.MDASH, 'g'), '–')
     .replace(new RegExp(HEX.NON_BREAKING_SPACE, 'g'), ' ');
 }
