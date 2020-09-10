a0367c28ab0d16e1bf059568285eefa948491f20
Jared Henderson <jared@netrivet.com>
2020-09-10 12:43:08 -0400

    evans: don't build alt-language podcasts

--- packages/evans/src/build/on-post-build.ts
+++ packages/evans/src/build/on-post-build.ts
@@ -4,10 +4,11 @@ import env from '@friends-library/env';
 import { eachEdition } from './helpers';
 import { podcast } from '../lib/xml';
 import { sendSearchDataToAlgolia } from './algolia';
+import { LANG } from '../env';
 
 const onPostBuild: GatsbyNode['onPostBuild'] = async () => {
-  eachEdition(({ document, edition }) => {
-    if (edition.audio) {
+  eachEdition(({ friend, document, edition }) => {
+    if (friend.lang === LANG && edition.audio) {
       const xmlHq = podcast(document, edition, `HQ`);
       fs.outputFileSync(`./public/${edition.audio.podcastRelFilepath(`HQ`)}`, xmlHq);
       const xmlLq = podcast(document, edition, `LQ`);
