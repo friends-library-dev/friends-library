f1329eca355f2058a0102f87cfcc17f99d517982
Jared Henderson <jared@netrivet.com>
2020-09-18 15:09:04 -0400

    app: changes for xcode 12.0.0

---
+++ packages/app/ios/BuildHelper.swift
@@ -0,0 +1,11 @@
+//
+//  BuildHelper.swift
+//  FriendsLibrary
+//
+//  Created by jared on 9/18/20.
+//
+//  Created after I got auto-updated to xcode 12.0.0 and everything broke
+//  followed some stuff on the internet, directing me to create this file
+//  tried to delete it, but build failed after deletion, so... ¯\_(ツ)_/¯
+
+import Foundation
